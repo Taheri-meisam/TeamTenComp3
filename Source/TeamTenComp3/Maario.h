1898e91a369c7bfb617036c8bf65c800d8ab9c37
SaraYohannes <89453142+SaraYohannes@users.noreply.github.com>
2022-03-16 15:11:44 +0100

    Overlap function added
    OverlapBegins and OverlapEnds functionality added

--- Source/TeamTenComp3/Maario.h
+++ Source/TeamTenComp3/Maario.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Character.h"
 #include "Maario.generated.h"
 
@@ -26,4 +27,21 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	/** Maario Charcter will point at pick-ups when he notices them, need support for animation, will need to be able to point at the pick-up */
+	/** Sphere component, when overlap begins, maario points in the right direction*/
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HitDetection")
+	USphereComponent* HitDetection{nullptr};
+
+	/** variable is false, when inside sphere component it will check if it is it is true*/
+	/** how to make mesh point in the right direction? vectors and a boom?*/
+
+	/** Overlap functions - on begin overlap the bool InPointingDistance turns true*/
+	UFUNCTION()
+	virtual void OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
+	/** Overlap functions - on ends overlap the bool InPointingDistance turns false*/
+	UFUNCTION()
+	virtual void OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 };
+
+	
