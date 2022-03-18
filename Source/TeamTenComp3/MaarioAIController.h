fd6f81720300c9cfe6e13716ad5c584267161f82
JohannaSordalen <89453144+JohannaSordalen@users.noreply.github.com>
2022-03-18 15:09:50 +0100

    updates

---
+++ Source/TeamTenComp3/MaarioAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MaarioAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAMTENCOMP3_API AMaarioAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
