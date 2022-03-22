d6023885792a239a750b1da696febb8820531772
TaisaKiarkhi <89452998+TaisaKiarkhi@users.noreply.github.com>
2022-03-22 15:33:20 +0100

    Health bar, ammo fixed

--- Source/TeamTenComp3/HealthWidget.h
+++ Source/TeamTenComp3/HealthWidget.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "PlayerTank.h"
 #include "HealthWidget.generated.h"
 
 /**
@@ -13,5 +14,21 @@ UCLASS()
 class TEAMTENCOMP3_API UHealthWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+
+
+	TWeakObjectPtr<APlayerTank>PlayerTankPtrs;
+
+	void PutNewOwner(APlayerTank* NewPlayerTank) {
+		PlayerTankPtrs = NewPlayerTank;
+	}
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UserInterface", meta = (BindWidget))
+		class UProgressBar* Progress_Bar;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "UserInterface", meta = (BindWidget))
+		class UTextBlock* Progress_Bar_Text_Block;
+
 };
